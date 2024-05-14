d09d09f879e15827f694e8e67bbcf6060e383e10
FaisalAlossaimi <fisail8484@gmail.com>
2024-05-14 22:59:58 +0300

    Faisal Alossaimi

---
+++ App.py
@@ -0,0 +1,33 @@
+import streamlit as st 
+import numpy as np
+import pandas as pd
+import streamlit as st
+from PIL import Image
+import plotly.express as px
+
+
+jdata = pd.read_csv('data\Jadarat_data.csv')
+
+
+st.image('images/—Pngtree—the young businessman walks up_5332299.png') 
+
+
+st.title('Know more of your dream careers')
+
+st.write("Desires are the plans of our desires, as well as within them exist the illustrations of our optimal professions. From youth dreams to grew ambitions the tapestry of desire jobs is as varied as the people that weave it. Yet what do these desires really require? What understandings can we obtain from the cumulative awareness when we search right into the world of profession passions? Invite to Know Even More of Your Desire Professions, where information brightens the course to recognizing. In this expedition, we venture right into the heart of human passion equipped with the devices of information evaluation. Our objective: to decipher the ins and outs of desire jobs coupled with clarify the aspects that shape them.")
+
+st.title("Avg Salary By jobs and Experiences")
+st.image('images/Untitled_page-0001.jpg') 
+st.write("The Manager Assistant role stands out as a prime option for those looking to expedite their career progression while securing a competitive salary. This position not only offers a robust average salary but also presents a pathway to professional growth achievable in a relatively short time frame. For individuals seeking to swiftly enhance their earning potential and advance within their chosen field, the Manager Assistant role proves to be an ideal fit.")
+
+st.title("The difference off salary between the private sector and the public sector")
+st.image('Images/Untitled_page-0002.jpg') 
+st.write('Contrary to popular belief, data often reveals that the public sector offers competitive average salaries, challenging the notion that the private sector pays more. Factors such as standardized salary structures, benefits, and specialized roles contribute to this discrepancy. While certain industries in the private sector may offer high earning potential, the public sector remains an attractive option for many professionals seeking stability and competitive compensation.')
+
+
+st.title("The difference off salary based on the size of the company")
+st.image('Images/Untitled 4_page-0004.jpg') 
+st.write('Company size strongly correlates with average salaries. As companies expand, they often allocate more resources to employee compensation. Larger firms, benefiting from economies of scale and facing intense competition, offer higher salaries to attract and retain talent. Additionally, the complexity of larger organizations demands specialized roles, further driving up average salaries. This trend underscores the importance of considering company size in understanding salary dynamics within organizations. ')
+
+st.title("Conclusion")
+st.write("In conclusion, the Manager Assistant role offers a promising pathway for career advancement and financial stability, challenging misconceptions about salary differentials between the public and private sectors. While the public sector provides competitive compensation packages, the influence of company size cannot be understated, with larger firms often offering higher salaries and specialized roles. As professionals navigate their career journeys, understanding these dynamics empowers informed decisions, ensuring alignment with personal goals and maximizing opportunities for success in an ever-evolving job market.")
