64d5fe2cc2c70befcdd14006f8103cb5a774c980
raseel4 <raseelsaad99@gmail.com>
2024-05-15 01:39:47 +0300

    Raseel Alhaqbani

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 import streamlit as st
 import pandas as pd
+import plotly.graph_objs as go
 
 # Load your dataset
 df = pd.read_csv('Data/Jadarat_data.csv')
@@ -10,10 +11,28 @@ st.markdown("In the heart of Saudi Arabia lies Riyadh, a bustling hub of economi
 # Regional Distribution of Job Postings
 st.subheader('Regional Distribution of Job Postings')
 st.write("In Riyadh, job postings flourish, painting a vivid picture of the city's economic vitality. With Riyadh leading the charge, it's evident that the capital serves as a beacon, attracting businesses and job seekers alike. This concentration of opportunities underscores Riyadh's pivotal role as an economic and administrative powerhouse in the region.")
-st.image(
-            "https://imgur.com/WjnMnEW.jpg",
-            width=400, # Manually Adjust the width of the image as per requirement
-        )
+# st.image(
+#             "https://imgur.com/WjnMnEW.jpg",
+#             width=400, # Manually Adjust the width of the image as per requirement
+#         )
+s = df[~pd.isnull(df['region'])]['region']
+chart = pd.value_counts(s).to_frame(name='data')
+chart['percent'] = (chart['data'] / chart['data'].sum()) * 100
+chart.index.name = 'labels'
+chart = chart.reset_index().sort_values(['data', 'labels'], ascending=[False, True])
+chart = chart[:100]
+charts = [go.Bar(x=chart['labels'].values, y=chart['data'].values, name='Frequency')]
+figure = go.Figure(data=charts, layout=go.Layout({
+    'barmode': 'group',
+    'legend': {'orientation': 'h'},
+    'title': {'text': 'region Value Counts'},
+    'xaxis': {'title': {'text': 'region'}},
+    'yaxis': {'title': {'text': 'Frequency'}}
+}))
+
+# Display the graph using Plotly in Streamlit
+st.plotly_chart(figure)
+
 st.markdown("""
 ### Insight:
 Riyadh leads in job postings, indicating a strong job market in the capital. This could be due to its status as an economic and administrative hub, attracting businesses and job seekers alike. Other regions with high job postings include Dammam, reflecting their importance as commercial and industrial centers.
@@ -22,10 +41,27 @@ Riyadh leads in job postings, indicating a strong job market in the capital. Thi
 # Gender Preference in Job Postings
 st.subheader('Gender Preference in Job Postings')
 st.write("In the realm of job postings, gender preferences occasionally surface, reflecting societal norms and industry traditions. While the majority of postings remain neutral in their gender specifications, there's a subtle undercurrent that warrants attention. Despite strides towards inclusivity, gender biases persist in certain sectors, influencing hiring practices and candidate preferences.")
-st.image(
-            "https://imgur.com/HYkJ4sM.jpg",
-            width=400, # Manually Adjust the width of the image as per requirement
-        )
+# st.image(
+#             "https://imgur.com/HYkJ4sM.jpg",
+#             width=400, # Manually Adjust the width of the image as per requirement
+#         )
+s = df[~pd.isnull(df['gender'])]['gender']
+chart = pd.value_counts(s).to_frame(name='data')
+chart['percent'] = (chart['data'] / chart['data'].sum()) * 100
+chart.index.name = 'labels'
+chart = chart.reset_index().sort_values(['data', 'labels'], ascending=[False, True])
+chart = chart[:100]
+charts = [go.Bar(x=chart['labels'].values, y=chart['data'].values, name='Frequency')]
+figure = go.Figure(data=charts, layout=go.Layout({
+    'barmode': 'group',
+    'legend': {'orientation': 'h'},
+    'title': {'text': 'gender Value Counts'},
+    'xaxis': {'title': {'text': 'gender'}},
+    'yaxis': {'title': {'text': 'Frequency'}}
+}))
+
+# Display the graph using Plotly in Streamlit
+st.plotly_chart(figure)
 
 st.markdown("""
 ### Insight:
