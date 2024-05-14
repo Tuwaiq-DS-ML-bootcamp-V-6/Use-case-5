71a123944df249b836a6895dacc00629d101945a
Saud <58867506+Saud122@users.noreply.github.com>
2024-05-14 23:33:31 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,17 +1,6 @@
-import numpy as np
-import pandas as pd
-import matplotlib.pyplot as plt
-import seaborn as sns
-from scipy.stats import zscore
-import dtale
+import streamlit as st
 
-from ipyvizzu import chart, Data, Config, Style
-
-
-
-Jadarat_Data = pd.read_csv('Jadarat_data.csv')
-
-dtale.show(Jadarat_Data)
+st.write('Hello world!')
 
 
 
