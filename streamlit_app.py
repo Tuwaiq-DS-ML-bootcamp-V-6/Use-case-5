9dcb9e29b598bb1750c48239a0fa3b93bb5ced14
Saud122 <58867506+Saud122@users.noreply.github.com>
2024-05-15 00:44:20 +0300

    Saud_A

--- streamlit_app.py
+++ streamlit_app.py
@@ -21,11 +21,15 @@ st.markdown("\n \n")
 
 
 st.markdown("""
+            
             <span style='color:black; font-weight:bold;'>
             Understanding regional job posting proportions in Saudi Arabia helps job seekers target their search efforts more effectively, 
             increasing their chances of finding relevant opportunities in areas with higher demand for their skills, 
             thus saving time and effort.</span>
             """, unsafe_allow_html=True )
+
+
+st.markdown("\n \n")
 
 st.markdown("""
     <span style='color:black; font-weight:bold;'> Q1)  What proportion of job postings is attributed to each region within the kingdom?</span>
@@ -36,3 +40,31 @@ st.markdown("\n \n")
 # Display an image from a local file
 st.image('image-1.png', caption='image-1.png', use_column_width=True)
 
+st.markdown("\n \n")
+st.markdown("\n \n")
+
+
+st.markdown("""
+            
+            <span style='color:black; font-weight:bold;'>
+            
+            Identifying if there's a gender preference specified in job postings allows job seekers to navigate towards workplaces
+            that prioritize diversity and inclusion, fostering environments where all individuals have equal opportunities for employment
+            and advancement..</span>
+            """, unsafe_allow_html=True )
+
+
+
+
+st.markdown("\n \n")
+
+st.markdown("""
+    <span style='color:black; font-weight:bold;'> Q2) Is there a gender preference indicated in the job postings?</span>
+""", unsafe_allow_html=True)
+
+st.markdown("\n \n")
+st.markdown("\n \n")
+
+
+# Display an image from a local file
+st.image('image-2.png', caption='image-2.png', use_column_width=True)
