d484e00eed21f00c26a4d742993259b41e3d87da
KiRBYwor1d <onlya7m4dz@gmail.com>
2024-05-14 20:59:15 +0300

    Ahmed Alhassan

---
+++ App.py
@@ -0,0 +1,106 @@
+import streamlit as st 
+st.image("pngwing.com.png")
+st.title("hey KiRBY you looking for a job?")
+
+# Define a function to style text
+def styled_text(text, background_color):
+    return f'<div style="background-color:{background_color}; padding: 20px; border-radius: 10px;">{text}</div>'
+
+# Define the text and background color
+text = "Sometimes, the process of searching for a job can leave us feeling awkward, unsure of where or when to look. But now, after navigating through that uncertainty, congratulations – you've finally arrived at your destination!"
+background_color = "White"
+
+# Display the styled text
+st.markdown(styled_text(text, background_color), unsafe_allow_html=True)
+
+st.title("Great news!")
+# Define a function to style text
+def styled_text(text, background_color):
+    return f'<div style="background-color:{background_color}; padding: 20px; border-radius: 10px;">{text}</div>'
+
+# Define the text and background color
+text = "You've found your buddy. Now, let's start searching for job opportunities in different locations. Looking for a job can be tricky, but you've made progress by reaching this point."
+background_color = "White"
+
+# Display the styled text
+st.markdown(styled_text(text, background_color), unsafe_allow_html=True)
+
+
+st.text(" ")
+st.image('pic1.jpg')
+
+
+
+# Define a function to style text
+def styled_text(text, background_color):
+    return f'<div style="background-color:{background_color}; padding: 20px; border-radius: 10px;">{text}</div>'
+
+# Define the text and background color
+text = "Based on the Figure provided, I suggest considering these cities. Start your day with a relaxing cup of coffee, embark on a road trip adventure, and keep an eye out for promising job prospects along the way."
+
+# Display the styled text
+st.markdown(styled_text(text, background_color), unsafe_allow_html=True)
+
+
+st.title("You can make it")
+st.text(" ")
+st.image('pic2.jpg')
+
+
+# Define a function to style text
+def styled_text(text, background_color):
+    return f'<div style="background-color:{background_color}; padding: 20px; border-radius: 10px;">{text}</div>'
+
+# Define the text and background color
+text = "Everyone has the opportunity to succeed in these major cities. Don't shy away from pursuing bigger opportunities; instead, embrace them and take bold steps forward."
+background_color = "white"
+
+# Display the styled text
+st.markdown(styled_text(text, background_color), unsafe_allow_html=True)
+
+st.title("Check out these awesome words!🌟")
+st.text(" ")
+st.image('pic3.jpg') 
+
+
+# Define a function to style text
+def styled_text(text, background_color):
+    return f'<div style="background-color:{background_color}; padding: 20px; border-radius: 10px;">{text}</div>'
+
+# Define the text and background color
+text = "They're like magic spells to boost your courage, hero. Let them fill your heart with bravery and your mind with determination as you journey through your adventures! "
+background_color = "white"
+
+# Display the styled text
+st.markdown(styled_text(text, background_color), unsafe_allow_html=True) 
+
+st.title("Conclusion")
+
+
+# Define a function to style text
+def styled_text(text, background_color):
+    return f'<div style="background-color:{background_color}; padding: 20px; border-radius: 10px;">{text}</div>'
+
+# Define the text and background color
+text = "Not everything i said will guarantee a job or a new adventure, buddy. But remember, the journey itself is full of surprises and possibilities. So, don't let anything discourage you from pursuing your dreams. Take a deep breath, put on a smile, and enjoy the ride. Have fun exploring new opportunities and finding your purpose. Catch you later, Hero! 🚀 "
+background_color = "white"
+
+# Display the styled text
+st.markdown(styled_text(text, background_color), unsafe_allow_html=True)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
