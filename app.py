ea2618965112fc7924fe790aaa7b3e2b453b387a
TariqWashtan <t.washtan@gmail.com>
2024-05-15 20:26:20 +0300

    Tariq Washtan

--- app.py
+++ app.py
@@ -8,12 +8,18 @@ def main():
     # Load the data
     df = pd.read_csv('Data/Jadarat_cleaned_data.csv') 
 
+    # Calculate the top 10 job titles
+    top_10_job_titles = df['job_title'].value_counts().nlargest(10).index.tolist()
+
+    # Filter the DataFrame to include only the top 10 job titles
+    df_top_10 = df[df['job_title'].isin(top_10_job_titles)]
+
     # Display the raw data
     st.subheader('Data Sample')
-    st.write(df)
+    st.write(df_top_10)
     
     # Create a bar chart
-    bar_chart = px.bar(df, x='job_title', title='Job Titles Distribution')
+    bar_chart = px.bar(df_top_10, x='job_title', title='Top 10 Job Titles Distribution')
 
     # Display the bar chart using st.plotly_chart
     st.plotly_chart(bar_chart)
