91b9f53f36eebdf7fcdc9ade4daa3441d63ee378
Fahad20m <109194138+Fahad20m@users.noreply.github.com>
2024-05-15 07:46:13 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,3 +1,11 @@
 import streamlit as st
 
-st.write('Hello world!')
+st.tittle('Jobs in Saudi Arabia')
+
+st.write("In the vibrant and ever-growing economy of Saudi Arabia, job postings serve as a gateway to exciting employment opportunities. With its rich cultural heritage, rapid modernization, and diverse sectors, Saudi Arabia offers a wide range of job prospects for both local residents and expatriates alike.")
+
+st.write("From bustling metropolises like Riyadh, Jeddah, and Dammam to emerging business hubs, Job seekers can explore positions at various levels, from entry-level roles to executive positions, catering to different skill sets and qualifications.")
+
+
+
+
