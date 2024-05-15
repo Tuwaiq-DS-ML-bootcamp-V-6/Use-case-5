cc129802972cf498700b49ac021d254b2b143e5a
Fahad20m <109194138+Fahad20m@users.noreply.github.com>
2024-05-15 10:02:03 +0300

    Update app.py

--- app.py
+++ app.py
@@ -2,11 +2,10 @@ import streamlit as st
 from PIL import Image 
 
 st.title('Jobs in Saudi Arabia')
-image_path = 'headr.png'
+image = Image.open('header.png')
 
 st.write("In the vibrant and ever-growing economy of Saudi Arabia, job postings serve as a gateway to exciting employment opportunities. With its rich cultural heritage, rapid modernization, and diverse sectors, Saudi Arabia offers a wide range of job prospects for both local residents and expatriates alike.")
 
-image_path = 'regions.jpg'
 st.write("From bustling metropolises like Riyadh, Jeddah, and Dammam to emerging business hubs, Job seekers can explore positions at various levels, from entry-level roles to executive positions, catering to different skill sets and qualifications.")
 
 
