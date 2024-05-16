f93050e6f94b5c1fdfa3482d3683722959d69d2b
ryyaon14 <ryyaon14@gmail.com>
2024-05-16 11:55:48 +0300

    Rayan

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ st.write("""
 In the dynamic job market of Saudi Arabia, young people are finding increasing opportunities to start their careers without prior experience. The "Distribution of Experience Requirements" chart reveals that over half of the job postings are for positions requiring zero years of experience. This indicates that companies are actively seeking fresh graduates and young job seekers, offering them a platform to begin their professional journeys.
 """)
 st.title('Distribution of Experience Requirements')
-st.image("C:\\Users\\ryyao\\Desktop\\Use-case-5\\Images\\Screenshot 2024-05-16 063929.png")
+st.image("Screenshot 2024-05-16 063929.png")
 st.write("""
 This trend highlights the supportive work environment in Saudi Arabia, where employers are willing to invest in young talent. Many companies provide training programs and mentorship opportunities to help new employees develop their skills and gain confidence. Such initiatives are crucial in helping young professionals feel valued and empowered as they transition into the workforce.
 """)
