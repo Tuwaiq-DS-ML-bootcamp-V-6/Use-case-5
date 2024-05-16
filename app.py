937378424a1f12535603a5cec34622416ce86277
ryyaon14 <ryyaon14@gmail.com>
2024-05-16 06:58:33 +0300

    Rayan ALshehri

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 import streamlit as st
 import pandas as pd
-data = pd.read_csv('C:\\Users\\ryyao\\Desktop\\Use-case-5\\Data\\Jadarat_data.csv')
-data = data.loc[:, ~data.columns.duplicated()]
+#data = pd.read_csv('C:\\Users\\ryyao\\Desktop\\Use-case-5\\Data\\Jadarat_data.csv')
+#data = data.loc[:, ~data.columns.duplicated()]
 st.title('Opportunities for Young Saudis')
 st.write("""
 In the dynamic job market of Saudi Arabia, young people are finding increasing opportunities to start their careers without prior experience. The "Distribution of Experience Requirements" chart reveals that over half of the job postings are for positions requiring zero years of experience. This indicates that companies are actively seeking fresh graduates and young job seekers, offering them a platform to begin their professional journeys.
